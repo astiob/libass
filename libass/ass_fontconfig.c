782111228d0828e493924453627950aec50d567a
Oleg Oshmyan <chortos@inbox.lv>
2014-02-11 00:06:05 +0000

    fontconfig: always fall back to Arial for VSFilter compatibility

--- libass/ass_fontconfig.c
+++ libass/ass_fontconfig.c
@@ -259,6 +259,9 @@ static char *select_font(ASS_Library *library, FCInstance *priv,
         !(r_family && strcasecmp((const char *) r_family, family) == 0) &&
         !(r_fullname && strcasecmp((const char *) r_fullname, family) == 0)) {
         char *fallback = (char *) (r_fullname ? r_fullname : r_family);
+        int use_arial = strcasecmp(family, "Arial");
+        if (use_arial)
+            fallback = "Arial";
         if (code) {
             ass_msg(library, MSGL_WARN,
                     "fontconfig: cannot find glyph U+%04X in font '%s', falling back to '%s'",
@@ -267,6 +270,12 @@ static char *select_font(ASS_Library *library, FCInstance *priv,
             ass_msg(library, MSGL_WARN,
                     "fontconfig: cannot find font '%s', falling back to '%s'",
                     family, fallback);
+        }
+        if (use_arial) {
+            free(retval);
+            retval = select_font(library, priv, "Arial", 0,
+                                 bold, italic, index, code);
+            goto error;
         }
     }
 
