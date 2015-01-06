fc378c14df5d27d181c778dd0699c56c170d35aa
Matt Oliver <protogonoi@gmail.com>
2015-01-06 17:14:33 +1100

    project: Cleanup config header.

--- SMP/version.h
+++
@@ -1,8 +0,0 @@
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libass 0.12.0"
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "0.12.0"
-
-/* Version number of package */
-#define VERSION "0.12.0"
