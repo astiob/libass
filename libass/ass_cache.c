5b09745afed74e6b2a2596ca8a2a1a416b1e7e93
rcombs <rcombs@rcombs.me>
2022-07-26 21:33:59 -0500

    ass_cache: add thread-safety

--- libass/ass_cache.c
+++ libass/ass_cache.c
@@ -302,7 +302,8 @@ typedef struct cache_item {
     const CacheDesc *desc;
     struct cache_item *next, **prev;
     struct cache_item *queue_next, **queue_prev;
-    size_t size, ref_count;
+    size_t size;
+    ASSAtomicSize ref_count;
 } CacheItem;
 
 struct cache {
@@ -316,6 +317,11 @@ struct cache {
     unsigned hits;
     unsigned misses;
     unsigned items;
+
+#if ENABLE_THREADS
+    ASSMutex mutex;
+    ASSCond  cond;
+#endif
 };
 
 #define CACHE_ALIGN 8
@@ -343,11 +349,28 @@ Cache *ass_cache_create(const CacheDesc *desc)
     cache->desc = desc;
     cache->map = calloc(cache->buckets, sizeof(CacheItem *));
     if (!cache->map) {
-        free(cache);
-        return NULL;
+        goto fail;
     }
 
+#if ENABLE_THREADS
+    if (!assi_mutex_init(&cache->mutex, true))
+        goto fail;
+
+    if (!assi_cond_init(&cache->cond))
+        goto failm;
+#endif
+
     return cache;
+
+#if ENABLE_THREADS
+failm:
+    assi_mutex_destroy(&cache->mutex);
+#endif
+
+fail:
+    free(cache->map);
+    free(cache);
+    return NULL;
 }
 
 void *ass_cache_get(Cache *cache, void *key, void *priv)
@@ -355,16 +378,20 @@ void *ass_cache_get(Cache *cache, void *key, void *priv)
     const CacheDesc *desc = cache->desc;
     size_t key_offs = CACHE_ITEM_SIZE + align_cache(desc->value_size);
     unsigned bucket = desc->hash_func(key, ASS_HASH_INIT) % cache->buckets;
+
+#if ENABLE_THREADS
+    assi_mutex_lock(&cache->mutex);
+#endif
+
     CacheItem *item = cache->map[bucket];
     while (item) {
         if (desc->compare_func(key, (char *) item + key_offs)) {
-            assert(item->size);
             if (!item->queue_prev || item->queue_next) {
                 if (item->queue_prev) {
                     item->queue_next->queue_prev = item->queue_prev;
                     *item->queue_prev = item->queue_next;
                 } else
-                    item->ref_count++;
+                    assi_inc_size(&item->ref_count);
                 *cache->queue_last = item;
                 item->queue_prev = cache->queue_last;
                 cache->queue_last = &item->queue_next;
@@ -372,7 +399,15 @@ void *ass_cache_get(Cache *cache, void *key, void *priv)
             }
             cache->hits++;
             desc->key_move_func(NULL, key);
-            item->ref_count++;
+            assi_inc_size(&item->ref_count);
+
+#if ENABLE_THREADS
+            while (!item->size)
+                assi_cond_wait(&cache->cond, &cache->mutex);
+
+            assi_mutex_unlock(&cache->mutex);
+#endif
+
             return (char *) item + CACHE_ITEM_SIZE;
         }
         item = item->next;
@@ -382,18 +417,23 @@ void *ass_cache_get(Cache *cache, void *key, void *priv)
     item = malloc(key_offs + desc->key_size);
     if (!item) {
         desc->key_move_func(NULL, key);
+#if ENABLE_THREADS
+        assi_mutex_unlock(&cache->mutex);
+#endif
         return NULL;
     }
-    item->cache = cache;
-    item->desc = desc;
     void *new_key = (char *) item + key_offs;
     if (!desc->key_move_func(new_key, key)) {
         free(item);
+#if ENABLE_THREADS
+        assi_mutex_unlock(&cache->mutex);
+#endif
         return NULL;
     }
-    void *value = (char *) item + CACHE_ITEM_SIZE;
-    item->size = desc->construct_func(new_key, value, priv);
-    assert(item->size);
+
+    item->cache = cache;
+    item->desc = desc;
+    item->size = 0;
 
     CacheItem **bucketptr = &cache->map[bucket];
     if (*bucketptr)
@@ -408,8 +448,29 @@ void *ass_cache_get(Cache *cache, void *key, void *priv)
     item->queue_next = NULL;
     item->ref_count = 2;
 
-    cache->cache_size += item->size;
     cache->items++;
+
+#if ENABLE_THREADS
+    assi_mutex_unlock(&cache->mutex);
+#endif
+
+    void *value = (char *) item + CACHE_ITEM_SIZE;
+    size_t size = desc->construct_func(new_key, value, priv);
+    assert(size);
+
+#if ENABLE_THREADS
+    assi_mutex_lock(&cache->mutex);
+#endif
+
+    item->size = size;
+
+    cache->cache_size += size;
+
+#if ENABLE_THREADS
+    assi_mutex_unlock(&cache->mutex);
+    assi_cond_broadcast(&cache->cond);
+#endif
+
     return value;
 }
 
@@ -433,7 +494,25 @@ void ass_cache_inc_ref(void *value)
         return;
     CacheItem *item = value_to_item(value);
     assert(item->size && item->ref_count);
-    item->ref_count++;
+    assi_inc_size(&item->ref_count);
+}
+
+static void cache_unlock_maybe_free(Cache *cache)
+{
+    bool destroy = !cache->items && !cache->map;
+
+#if ENABLE_THREADS
+    assi_mutex_unlock(&cache->mutex);
+#endif
+
+    if (destroy) {
+#if ENABLE_THREADS
+        assi_mutex_destroy(&cache->mutex);
+        assi_cond_destroy(&cache->cond);
+#endif
+
+        free(cache);
+    }
 }
 
 void ass_cache_dec_ref(void *value)
@@ -442,25 +521,39 @@ void ass_cache_dec_ref(void *value)
         return;
     CacheItem *item = value_to_item(value);
     assert(item->size && item->ref_count);
-    if (--item->ref_count)
+    if (assi_dec_size(&item->ref_count))
         return;
 
     Cache *cache = item->cache;
-    if (cache) {
-        if (item->next)
-            item->next->prev = item->prev;
-        *item->prev = item->next;
 
-        cache->items--;
-        cache->cache_size -= item->size;
-    }
+#if ENABLE_THREADS
+    assi_mutex_lock(&cache->mutex);
+#endif
+
+    if (item->next)
+        item->next->prev = item->prev;
+    *item->prev = item->next;
+
+    cache->cache_size -= item->size;
+    cache->items--;
+
+    cache_unlock_maybe_free(cache);
+
     destroy_item(item->desc, item);
 }
 
 void ass_cache_cut(Cache *cache, size_t max_size)
 {
-    if (cache->cache_size <= max_size)
+#if ENABLE_THREADS
+    assi_mutex_lock(&cache->mutex);
+#endif
+
+    if (cache->cache_size <= max_size) {
+#if ENABLE_THREADS
+        assi_mutex_unlock(&cache->mutex);
+#endif
         return;
+    }
 
     do {
         CacheItem *item = cache->queue_first;
@@ -469,7 +562,7 @@ void ass_cache_cut(Cache *cache, size_t max_size)
         assert(item->size);
 
         cache->queue_first = item->queue_next;
-        if (--item->ref_count) {
+        if (assi_dec_size(&item->ref_count)) {
             item->queue_prev = NULL;
             continue;
         }
@@ -486,11 +579,19 @@ void ass_cache_cut(Cache *cache, size_t max_size)
         cache->queue_first->queue_prev = &cache->queue_first;
     else
         cache->queue_last = &cache->queue_first;
+
+#if ENABLE_THREADS
+    assi_mutex_unlock(&cache->mutex);
+#endif
 }
 
 void ass_cache_stats(Cache *cache, size_t *size, unsigned *hits,
                      unsigned *misses, unsigned *count)
 {
+#if ENABLE_THREADS
+    assi_mutex_lock(&cache->mutex);
+#endif
+
     if (size)
         *size = cache->cache_size;
     if (hits)
@@ -499,21 +600,35 @@ void ass_cache_stats(Cache *cache, size_t *size, unsigned *hits,
         *misses = cache->misses;
     if (count)
         *count = cache->items;
+
+#if ENABLE_THREADS
+    assi_mutex_unlock(&cache->mutex);
+#endif
 }
 
 void ass_cache_empty(Cache *cache)
 {
+#if ENABLE_THREADS
+    assi_mutex_lock(&cache->mutex);
+#endif
+
     for (int i = 0; i < cache->buckets; i++) {
         CacheItem *item = cache->map[i];
         while (item) {
             assert(item->size);
             CacheItem *next = item->next;
+
             if (item->queue_prev)
-                item->ref_count--;
-            if (item->ref_count)
-                item->cache = NULL;
-            else
+                assi_dec_size(&item->ref_count);
+
+            if (!item->ref_count) {
+                if (item->next)
+                    item->next->prev = item->prev;
+                *item->prev = item->next;
+
                 destroy_item(cache->desc, item);
+                cache->items--;
+            }
             item = next;
         }
         cache->map[i] = NULL;
@@ -521,14 +636,25 @@ void ass_cache_empty(Cache *cache)
 
     cache->queue_first = NULL;
     cache->queue_last = &cache->queue_first;
-    cache->items = cache->hits = cache->misses = cache->cache_size = 0;
+    cache->hits = cache->misses = cache->cache_size = 0;
+
+#if ENABLE_THREADS
+    assi_mutex_unlock(&cache->mutex);
+#endif
 }
 
 void ass_cache_done(Cache *cache)
 {
     ass_cache_empty(cache);
+
+#if ENABLE_THREADS
+    assi_mutex_lock(&cache->mutex);
+#endif
+
     free(cache->map);
-    free(cache);
+    cache->map = NULL;
+
+    cache_unlock_maybe_free(cache);
 }
 
 // Type-specific creation function
